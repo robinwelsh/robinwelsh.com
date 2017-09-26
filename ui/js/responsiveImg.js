806d1c3005c1f2b0ba359c087c42dd9990afd977
Robin Welsh <robin@manzama.com>
2017-09-26 14:29:54 -0700

    Rewrote responsiveImg.js to reflect HTML5 standard
    Now uses <img srcset> instead of interim JS solution.

--- ui/js/responsiveImg.js
+++ ui/js/responsiveImg.js
@@ -1,15 +1,15 @@
 // requires picturefill
-function responsiveImg(output,name,alt) {
-    var $pfOutput;
-    $pfOutput = '<span data-picture data-alt="' + alt + '">';
-		$pfOutput += '<span data-src="' + name + '-small.jpg"     data-media="(min-width: 320px) and (-webkit-min-device-pixel-ratio: 1)"></span>';
-		$pfOutput += '<span data-src="' + name + '-medium.jpg"></span>';
-		$pfOutput += '<span data-src="' + name + '-large.jpg"      data-media="(min-width: 768px)"></span>';
-		$pfOutput += '<span data-src="' + name + '-extralarge.jpg" data-media="(min-width: 1025px)"></span>';
-		$pfOutput += '<noscript>';
-			$pfOutput += '<img src="' + name + '-large.jpg" alt="' + alt + '">/';
-		$pfOutput += '</noscript>';
-	$pfOutput += '</span>';
-    document.write($pfOutput);
+function imgSrcSet(output,name,alt) {
+    var $srcSetOutput;
+    	$srcSetOutput = '<img srcset="' + name + '-small.jpg 320w,';
+    	$srcSetOutput += name + '-medium.jpg 460w,';
+    	$srcSetOutput += name + '-large.jpg 768w,';
+    	$srcSetOutput += name + '-extralarge.jpg 1025w"';
+    	$srcSetOutput += 'sizes="(max-width:320px) 100%,';
+    	$srcSetOutput += '(max-width:640px) 100%,';
+    	$srcSetOutput += '(max-width:768px) 100%,';
+    	$srcSetOutput += '100%"';
+    	$srcSetOutput += 'src="' + name + '-extralarge.jpg" alt="' + alt + '">';
+    document.write($srcSetOutput);
     return true;
 };
