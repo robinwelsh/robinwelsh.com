f93f315beb4e0feed89b960114ac8c011edca910
Robin Welsh <robin@manzama.com>
2017-09-27 16:29:15 -0700

    Redesign progress
    * piTV images slightly more compressed
    * SCSS redesign updates in progress
    * JS additions for new UI

---
+++ ui/js/ui.js
@@ -0,0 +1,27 @@
+$("#navMain").ready(function() {
+	$("#navMain").hide();
+});
+
+$( "#navToggle" ).click(function() {
+	if ($("#navMain").hasClass("shown")) {
+		$("#navMain").toggleClass("shown");
+		setTimeout(function(){ $("#navMain").hide(); }, 200);
+	} else {
+		$("#navMain").show();
+		$("#navMain").toggleClass("shown");
+	};
+ 	$( "#navToggle" ).toggleClass( "close" );
+});
+
+$( ".artboards.swap2" ).click(function() {
+	$( this ).toggleClass("toggle");
+});
+
+$( ".artboards.rotate3" ).click(function() {
+	var first = $( this ).children(".first");
+	var second = $( this ).children(".second");
+	var third = $( this ).children(".third");
+	first.removeClass("first").addClass("third");
+	second.removeClass("second").addClass("first")
+	third.removeClass("third").addClass("second");
+});
